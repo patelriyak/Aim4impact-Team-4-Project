b5a6b8342c896d7322f1d414b3b6e1b542526492
Zankruta <zankrutac@gmail.com>
2024-05-13 22:04:34 -0400

    added main navigation bar

--- Front-end/src/components/MainNavigation.jsx
+++ Front-end/src/components/MainNavigation.jsx
@@ -1,3 +1,37 @@
+import { NavLink, Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+import logo from "../assets/Logo.jpg";
+
 export default function MainNavigation() {
-  return <header>Main naviagtion</header>;
+  return (
+    <header className={classes["nav-header"]}>
+      <div>
+        <img src={logo} width="105" height="36" alt="Maxwell" />
+      </div>
+      <nav className={classes.navigation}>
+        <ul className={classes["nav-links"]}>
+          <li>
+            <NavLink to="">Home</NavLink>
+          </li>
+          <li>
+            <NavLink>Features</NavLink>
+          </li>
+          <li>
+            <NavLink to="medication">Medication</NavLink>
+          </li>
+          <li>
+            <NavLink>About Us</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className={classes.cta}>
+        <Link to="login" className={classes["sign-in-button"]}>
+          Sign In
+        </Link>
+        <Link to="sign-up" className={classes["sign-up-button"]}>
+          Sign Up
+        </Link>
+      </div>
+    </header>
+  );
 }
