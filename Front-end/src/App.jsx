f9b5e908c548d45a563e7073b5b821064d5356f6
Zankruta <zankrutac@gmail.com>
2024-05-13 21:15:51 -0400

    added components and main navigation structure

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,5 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./pages/Root";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "medication", element: <MedicationPage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "sign-up", element: <SignupPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Home page</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ Front-end/src/components/MainNavigation.jsx
@@ -0,0 +1,3 @@
+export default function MainNavigation() {
+  return <header>Main naviagtion</header>;
+}
